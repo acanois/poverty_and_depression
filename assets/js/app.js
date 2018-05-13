a22c808a854a4441a8db720f02d08ca48d30033a
acanois <david.richter42@gmail.com>
2018-05-13 09:41:00 -0500

    fixing broken scales

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,30 +7,24 @@ d3.csv('../../data/poverty_depression.csv', (err, depressionData) => {
     if (err) console.log(err)
 
     // Set up the canvas
-    var body = d3.select('body')
-    var margin = {top: 20, right: 20, bottom: 30, left: 40}
-    var width = 960 - margin.left - margin.right
-    var height = 500 - margin.top - margin.bottom
+    const body = d3.select('body')
+    const margin = {top: 20, right: 20, bottom: 30, left: 40}
+    const width = 960 - margin.left - margin.right
+    const height = 500 - margin.top - margin.bottom
 
-    var xScale = d3.scaleLinear()
-                   .domain([
-                       d3.min([0, d3.min(depressionData, d => { return d.belowPoverty })]),
-                    //    d3.max([0, d3.max(depressionData, d => { return d.belowPoverty})])
-                    48
-                   ])
-                   .range([0, width])
-    console.log(d3.min([0, d3.min(depressionData, d => { return d.belowPoverty})]))
-    console.log(d3.max([0, d3.max(depressionData, d => { return d.belowPoverty})]))
+    const xScale = d3.scaleLinear()
+        .range([0, width])
+        .domain(d3.extent(depressionData, d => d.percentDepressed)).nice()
     
-    var yScale = d3.scaleLinear()
-                   .domain([ 0, 30
-                    //    d3.min([0, d3.min(depressionData, d => { return d.percentDepressed })]),
-                    //    d3.max([0, d3.max(depressionData, d => { return d.percentDepressed })])
+    const yScale = d3.scaleLinear()
+                   .domain([ //0, 30
+                       d3.min([0, d3.min(depressionData, d => { return d.percentDepressed })]),
+                       d3.max([0, d3.max(depressionData, d => { return d.percentDepressed })])
                    ])
                    .range([height, 0])
 
     // SVG
-    var svg = body.append('svg')
+    const svg = body.append('svg')
                 .attr('height', height + margin.top + margin.bottom)
                 .attr('width', width + margin.left + margin.right)
                 .append('g')
@@ -38,15 +32,15 @@ d3.csv('../../data/poverty_depression.csv', (err, depressionData) => {
 
 
     // X-axis
-    var xAxis = d3.axisBottom()
+    const xAxis = d3.axisBottom()
                 .scale(xScale)
 
     // Y-axis
-    var yAxis = d3.axisLeft()
+    const yAxis = d3.axisLeft()
                 .scale(yScale)
                 
     // Circles
-    var circlesGroup = svg.selectAll('circle')
+    const circlesGroup = svg.selectAll('circle')
                     .data(depressionData)
                     .enter()
                     .append('circle')
@@ -62,28 +56,14 @@ d3.csv('../../data/poverty_depression.csv', (err, depressionData) => {
                     .attr('stroke-width',1)
                     .attr('fill', 'blue')
                     .attr('opacity', 0.75)
-                    .on('mouseover', function () {
-                        d3.select(this)
-                        .transition()
-                        .duration(500)
-                        .attr('r', 20)
-                        .attr('stroke-width', 3)
-                    })
-                    .on('mouseout', function () {
-                    d3.select(this)
-                        .transition()
-                        .duration(500)
-                        .attr('r', 5)
-                        .attr('stroke-width',1)
-                    })
     
-    var toolTip = d3.select('body').append('div').attr('class', 'tooltip')
-    var createToolTip = function () {
+    const toolTip = d3.select('body').append('div').attr('class', 'tooltip')
+    const createToolTip = function () {
         return d3.select('body').append('div').attr('class', 'tooltip')
     }
 
     circlesGroup.on('mouseover', (d, i) => {
-        var newToolTip = createToolTip()
+        const newToolTip = createToolTip()
         newToolTip.style('display', 'block')
         newToolTip.html('<p>Test</p>')
                   .style("left", d3.event.pageX + "px")
