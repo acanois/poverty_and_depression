614bce39027550106453c1b57bd1d7cda4e0fa08
acanois <david.richter42@gmail.com>
2018-05-13 13:25:30 -0500

    add tooltips

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,99 +4,86 @@
 // =========
 // Follow your written instructions and create a scatter plot with D3.js.
 d3.csv('../../data/poverty_depression.csv', (err, depressionData) => {
-    if (err) console.log(err)
+  if (err) console.log(err)
 
-    // Set up the canvas
-    const margin = {
-        top: 20, 
-        right: 20, 
-        bottom: 30, 
-        left: 40
-    }
-    
-    const width = 960 - margin.left - margin.right
-    const height = 500 - margin.top - margin.bottom
+  // Set up the canvas
+  var margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 40
+  }
 
-    // SVG
-    const svg = d3.select('.chart')
-                .append('svg')
-                .attr('height', height + margin.top + margin.bottom)
-                .attr('width', width + margin.left + margin.right)
-                .append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  var width = 960 - margin.left - margin.right
+  var height = 500 - margin.top - margin.bottom
 
-    const xScale = d3.scaleLinear()
-        .range([0, width])
-        .domain(d3.extent(depressionData, d => d.percentDepressed)).nice()
+  // SVG
+  var svg = d3.select('.chart')
+    .append('svg')
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-    const yScale = d3.scaleLinear()
-        .range([height, 0])
-        .domain(d3.extent(depressionData, d => d.percentDepressed)).nice()
+  var xScale = d3.scaleLinear()
+    .range([0, width])
+    .domain(d3.extent(depressionData, d => d.percentDepressed)).nice()
 
-    // X-axis
-    const xAxis = d3.axisBottom(xScale)
-    // Y-axis
-    const yAxis = d3.axisLeft(yScale)
-                
-    // Circles
-    const circlesGroup = svg.selectAll('circle')
-                    .data(depressionData)
-                    .enter()
-                    .append('circle')
-                    .attr('cx', d => xScale(d.belowPoverty))
-                    .attr('cy', d => yScale(d.percentDepressed)) 
-                    .attr('r','8')
-                    .attr('fill', 'blue')
-                    .attr('opacity', 0.75)
+  var yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain(d3.extent(depressionData, d => d.percentDepressed)).nice()
 
-    var toolTip = d3.select('.tooltip')
-        .append('div')
-        .style('display', 'none')
-        .classed('tooltip', true)
+  // X-axis
+  var xAxis = d3.axisBottom(xScale)
+  // Y-axis
+  var yAxis = d3.axisLeft(yScale)
 
-    circlesGroup.on('mouseover', function (d) {
-        toolTip.style('display', 'block')
-            .html(`<p>Test</p>`)
-            .style('left', d3.event.pageX + 'px')
-            .style('top', d3.event.pageY + 'py')
-        })
-        .on('moueout', function () {
-            toolTip.style('display', 'none')
-        })
-    
-    // const toolTip = d3.select('body').append('div').attr('class', 'tooltip')
-    // const createToolTip = function () {
-    //     return d3.select('body').append('div').attr('class', 'tooltip')
-    // }
+  // Circles
+  var circlesGroup = svg.selectAll('circle')
+    .data(depressionData)
+    .enter()
+    .append('circle')
+    .attr('cx', d => xScale(d.belowPoverty))
+    .attr('cy', d => yScale(d.percentDepressed))
+    .attr('r', '8')
+    .attr('fill', 'blue')
+    .attr('opacity', 0.75)
 
-    // circlesGroup.on('mouseover', function (d, i) {
-    //     const newToolTip = createToolTip()
-    //     newToolTip.style('display', 'block')
-    //     newToolTip.html('<p>Test</p>')
-    //               .style("left", d3.event.pageX + "px")
-    //               .style("top", d3.event.pageY + "px");
-    // })
-    // .on('mouseout',  function (d, i) {
-    //     toolTip.hide(d)
-    // })
-    
-    // X-axis
-    svg.append('g')
-        .attr('class','axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis)
-        .append('text') // X-axis Label
-        .attr('class','label')
-        .attr('y', -10)
-        .attr('x', width)
+  var toolTip = d3.select("body")
+    .append("div")
+    .style("display", "none")
+    .classed('tooltip', true)
 
-    // Y-axis
-    svg.append('g')
-        .attr('class', 'axis')
-        .call(yAxis)
-        .append('text') // y-axis Label
-        .attr('class','label')
-        .attr('transform','rotate(-90)')
-        .attr('x',0)
-        .attr('y',5)
+  circlesGroup.on('mouseover', function(d) {
+      toolTip.style('display', 'block')
+        .html(`
+          <p>${d.state}:</p>
+          <p>Below Poverty: ${d.belowPoverty}%</p>
+          `)
+        .style('left', d3.event.pageX + 'px')
+        .style('top', d3.event.pageY + 'py')
+    })
+    .on('mouseout', function() {
+      toolTip.style('display', 'none')
+    })
+
+  // X-axis
+  svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
+    .append('text') // X-axis Label
+    .attr('class', 'label')
+    .attr('y', -10)
+    .attr('x', width)
+
+  // Y-axis
+  svg.append('g')
+    .attr('class', 'axis')
+    .call(yAxis)
+    .append('text') // y-axis Label
+    .attr('class', 'label')
+    .attr('transform', 'rotate(-90)')
+    .attr('x', 0)
+    .attr('y', 5)
 })
